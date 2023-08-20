32289711e135f7f76ceea5cc247ff5113c796091
marinamun <marinacerejidoo@gmail.com>
2023-08-20 13:30:27 +0200

    Created the basic HTML

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,34 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="container">
-      <hi>WeatherApp</hi>
+      <form>
+        <input type="search" placeholder="Enter a city..."/>
+        <input type="submit" value="Search"/>
+      </form>
+
+
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 7:00</li>
+        <li>Mostly cloudy</li>
+      </ul>
+
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="weather"
+          />
+          6ºC
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation</li>
+            <li>Humidity</li>
+            <li>Wind</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
