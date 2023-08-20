fe0f536c1096bdd770e4080a99cd23a25f56eddc
marinamun <marinacerejidoo@gmail.com>
2023-08-20 14:46:50 +0200

    Added the basic CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import "./styles.css";
 import React from "react";
 import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <Weather />
+      <footer>
+        <p>
+          This proyect was coded by Marina Muñoz, open-sourced on{" "}
+          <a href="https://github.com/marinamun/react-weather-app">GitHub</a>
+        </p>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -3,15 +3,20 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="container">
+    <div className="weather">
       <form>
-        <input type="search" placeholder="Enter a city..."/>
-        <input type="submit" value="Search"/>
+        <div className="row">
+          <div className="col-9">
+            <input type="search" placeholder="Enter a city..." autoFocus="on" />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary w-100"/>
+          </div>
+        </div>
       </form>
 
-
       <h1>New York</h1>
-      <ul>
+      <ul className="main-info">
         <li>Wednesday 7:00</li>
         <li>Mostly cloudy</li>
       </ul>
@@ -21,8 +26,10 @@ export default function Weather() {
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="weather"
+            className="main-img"
           />
-          6ºC
+          <span className="temperature">6</span>
+          <span className="unit">ºC</span>
         </div>
         <div className="col-6">
           <ul>
